a39db0b18afbef0a2f259abe748a549ba88cc8cc
Dare <patryk.zielonka1204@gmail.com>
2022-06-09 18:55:41 +0200

    Updating the branch - Dare

---
+++ RPG_group_alpha/textures/division_and_organisation.py
@@ -0,0 +1,43 @@
+from RPG_group_alpha import parent_file as p_f
+
+img = p_f.pygame.image.load('RPG_group_alpha/textures/player_weapons1-2.png')
+
+playeranimation = {
+    'breathing': [p_f.pygame.transform.scale(img.subsurface(p_f.pygame.Rect(0*24, 0, 24, 24)), (100, 100)),
+                  p_f.pygame.transform.scale(img.subsurface(p_f.pygame.Rect(1 * 24, 0, 24, 24)), (100, 100))],
+
+    'attack': [p_f.pygame.transform.scale(img.subsurface(p_f.pygame.Rect(2 * 24, 0, 24, 24)), (100, 100)),
+                             p_f.pygame.transform.scale(img.subsurface(p_f.pygame.Rect(3 * 24, 0, 24, 24)), (100, 100)),
+                             p_f.pygame.transform.scale(img.subsurface(p_f.pygame.Rect(4 * 24, 0, 24, 24)), (100, 100)),
+                             p_f.pygame.transform.scale(img.subsurface(p_f.pygame.Rect(5 * 24, 0, 24, 24)), (100, 100))]
+}
+
+
+spearanimation = {
+    'breathing': [p_f.pygame.transform.scale(img.subsurface(p_f.pygame.Rect(0 * 24, 24, 24, 24)), (100, 100)),
+                  p_f.pygame.transform.scale(img.subsurface(p_f.pygame.Rect(1 * 24, 24, 24, 24)), (100, 100))],
+
+    'attack': [p_f.pygame.transform.scale(img.subsurface(p_f.pygame.Rect(2 * 24, 24, 24, 24)), (100, 100)),
+               p_f.pygame.transform.scale(img.subsurface(p_f.pygame.Rect(3 * 24, 24, 24, 24)), (100, 100)),
+               p_f.pygame.transform.scale(img.subsurface(p_f.pygame.Rect(4 * 24, 24, 24, 24)), (100, 100)),
+               p_f.pygame.transform.scale(img.subsurface(p_f.pygame.Rect(5 * 24, 24, 24, 24)), (100, 100))]
+}
+
+
+knifeanimation = {
+    'breathing': [p_f.pygame.transform.scale(img.subsurface(p_f.pygame.Rect(0 * 24, 48, 24, 24)), (100, 100)),
+                  p_f.pygame.transform.scale(img.subsurface(p_f.pygame.Rect(1 * 24, 48, 24, 24)), (100, 100))],
+
+    'attack': [p_f.pygame.transform.scale(img.subsurface(p_f.pygame.Rect(2 * 24, 48, 24, 24)), (100, 100)),
+               p_f.pygame.transform.scale(img.subsurface(p_f.pygame.Rect(3 * 24, 48, 24, 24)), (100, 100)),
+               p_f.pygame.transform.scale(img.subsurface(p_f.pygame.Rect(4 * 24, 48, 24, 24)), (100, 100)),
+               p_f.pygame.transform.scale(img.subsurface(p_f.pygame.Rect(5 * 24, 48, 24, 24)), (100, 100))]
+}
+
+
+delays = {'playeranimation breathing': 333,
+          'playeranimation attack': 200,
+          'spearanimation breathing': 333,
+          'spearanimation attack': 200,
+          'knifeanimation breathing': 333,
+          'knifeanimation attack': 200}
